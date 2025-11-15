01a4fc7a51ccead5e830617bfec08b18869d5279
Lord.Aslan <aslani80.m@gmail.com>
2025-11-15 12:01:08 +0330

    update UserData model & made migrations

--- downloader/models.py
+++ downloader/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class UserData(models.Model):
-    account_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100)
+    account_name = models.CharField(max_length=100, null=True, blank=True)
+    username = models.CharField(max_length=100, null=True, blank=True)
     requested_url = models.TextField()
 
     def __str__(self):
