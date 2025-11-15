7248fde98372974b4e7b0601dc75abc2a16c627a
Lord.Aslan <aslani80.m@gmail.com>
2025-11-15 11:27:29 +0330

    create UserData model

--- downloader/models.py
+++ downloader/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class UserData(models.Model):
+    account_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    requested_url = models.TextField()
+
+    def __str__(self):
+        return f"{self.username} | {self.account_name}"
