af9f1d0ae57cfe96d353575e8c7488e8e18fd704
Lord.Aslan <aslani80.m@gmail.com>
2025-11-16 11:26:47 +0330

    change the way of using .env file

--- spotify_downloader_bot/settings.py
+++ spotify_downloader_bot/settings.py
@@ -16,12 +16,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read .env file
+ENV_FILE = dotenv_values(".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = dotenv_values(".env")["SECRET_KEY"]
+SECRET_KEY = ENV_FILE["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
