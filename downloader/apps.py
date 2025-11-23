e4ef876e40f8081c00b8a11b0a581791499613b4
Lord.Aslan <aslani80.m@gmail.com>
2025-11-23 12:33:05 +0330

    modify apps.py to run the bot when server is running

--- downloader/apps.py
+++ downloader/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+import os
+import threading
 
 
 class DownloaderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'downloader'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "downloader"
+
+    def ready(self):
+        if os.environ.get("RUN_MAIN") != "true":
+            return
+
+        bot_thread = threading.Thread(target=self.start_bot, daemon=True)
+        bot_thread.start()
+
+    def start_bot(self):
+        from .bot import start_bot
+
+        start_bot()
